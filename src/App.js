42da56289c207b50d52e0fa6b234a83aaca9431a
trungtt2162 <trungtt.212@gmail.com>
2023-08-21 15:38:51 +0700

    21/08/23

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import MyComponent from './components/MyComponent';
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Trương Tấn Trung Hello
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+    <div>
+      Trương Tấn Trung Welcome
+      <MyComponent></MyComponent>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Trương Tấn Trung Welcome
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
   );
 }
 
