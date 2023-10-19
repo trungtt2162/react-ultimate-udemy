96252cadd93d484056d1532ff1b1d0d4616c7181
trungtt2162 <trungtt.212@gmail.com>
2023-10-20 00:08:03 +0700

    asios-customize

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,9 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import './ManageUser.scss';
 import { BsCardImage } from "react-icons/bs";
-import axios from 'axios';
 import { toast } from 'react-toastify';
-
+import { postCreateNewUser } from './../../../services/apiServices';
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
     const handleClose = () => {
@@ -49,23 +48,14 @@ const ModalCreateUser = (props) => {
             toast.error('Invalid Password !!!')
             return;
         }
-        //alert('Save?')
-        //call api ips?
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
-
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-        console.log('>>>', res.data);
-        if (res.data && res.data.EC === 0) {
-            toast.success(res.data.EM)
+        let data = await postCreateNewUser(email, password, username, role, image);
+        console.log('>>>', data);
+        if (data && data.EC === 0) {
+            toast.success(data.EM)
             handleClose();
         }
-        if (res.data && res.data.EC !== 0) {
-            toast.error(res.data.EM);
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
         }
     }
     return (
